aa8afa66b1f2b0929ad9768494ad4e535c47a86b
2020-DEV-133 <debintomdt@gmail.com>
2020-11-04 13:57:26 +0000

    home bottom nav design added

--- app/src/main/java/com/meg/githubbrowser/HomeActivity.kt
+++ app/src/main/java/com/meg/githubbrowser/HomeActivity.kt
@@ -2,10 +2,27 @@ package com.meg.githubbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottom_nav_home.setupWithNavController(navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this,
+            R.id.nav_host_fragment_home).navigateUp() || super.onSupportNavigateUp()
     }
 }
